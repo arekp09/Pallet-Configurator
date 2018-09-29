8eca74bbb456b6dc85282799779437ca3b800d64
Arkadiusz Pokrzywa <arek09@gmail.com>
2018-09-29 20:42:57 +0100

    Class 'Pallet' added

---
+++ PalletConfig.Library/Pallet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalletConfig.Library
+{
+    public class Pallet
+    {
+        public int PalletSizeX { get; set; }
+        public int PalletSizeY { get; set; }
+        public int PalletSizeZ { get; set; }
+        public int BoxSizeX { get; set; }
+        public int BoxSizeY { get; set; }
+        public int BoxSizeZ { get; set; }
+        public int BoxWeight { get; set; }
+        public int PalletHeight { get; set; }
+        public int PalletWeight { get; set; }
+    }
+}

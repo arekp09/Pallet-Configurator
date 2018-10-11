23022aed9969aa2cee792810757c8a3878f1824b
Arkadiusz Pokrzywa <arek09@gmail.com>
2018-10-11 10:19:11 +0100

    Rotation added (OrbitControls)

--- PalletConfig.Web/wwwroot/js/canvas.js
+++ PalletConfig.Web/wwwroot/js/canvas.js
@@ -3,7 +3,7 @@
         return window.requestAnimationFrame;
     })();
 
-    var camera, scene, renderer;
+    var camera, scene, renderer, controls;
     var meshTop;
     var position = new Coordinates(0, 0, 0);
     var palletSize = new Coordinates(1, 0.2, 1.5);
@@ -17,7 +17,7 @@
         // Set camera
         camera = new THREE.PerspectiveCamera(70, canvasWidth / canvasHeight, 0.01, 100);
         camera.position.z = 2;
-        camera.position.y = 0.5;
+        //camera.position.y = 0.5;
         //camera.position.x = -1;
 
         // Set scene
@@ -34,15 +34,20 @@
 
         createPallet();
 
+        controls = new THREE.OrbitControls(camera);
+        controls.addEventListener('change', renderer);
+
         renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         renderer.setSize(canvasWidth, canvasHeight);
-        canvas.appendChild(renderer.domElement);       
+        canvas.appendChild(renderer.domElement);    
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+
     }
 
     function animate() {
         if (isAnimationOn) {
             requestAnimationFrame(animate);
-            meshTop.rotation.y += 0.005;
             renderer.render(scene, camera);
         }
     }
@@ -66,29 +71,35 @@
         meshTop.position.set(position.X, position.Y, position.Z);        
         
         // Create bottom of pallet
-        var meshBottom = new THREE.Mesh(geometryTop, material);
-        scene.add(meshBottom);
-        meshBottom.position.set(position.X, position.Y - (palletSize.Y * 0.95), position.Z);
-        meshTop.add(meshBottom);
+        var geometryBottom = new THREE.BoxGeometry(palletSize.X * 0.1, palletSize.Y * 0.05, palletSize.Z);
+        for (var i = 0; i < 3; i++) {
+            var meshBottom = new THREE.Mesh(geometryBottom, material);
+            scene.add(meshBottom);
+            meshBottom.position.set(position.X + (palletSize.X * 0.45) * (i-1), position.Y - (palletSize.Y * 0.95), position.Z);
+            meshTop.add(meshBottom);
+        }
         
         // Create middle bricks
-        var geometryBrick = new THREE.BoxGeometry(palletSize.X * 0.1, (palletSize.Y * 0.9), palletSize.Z);
-        var meshBrick1 = new THREE.Mesh(geometryBrick, material);
-        var meshBrick2 = new THREE.Mesh(geometryBrick, material);
-        var meshBrick3 = new THREE.Mesh(geometryBrick, material);
-        scene.add(meshBrick1);
-        scene.add(meshBrick2);
-        scene.add(meshBrick3);
-        meshTop.add(meshBrick1);
-        meshTop.add(meshBrick2);
-        meshTop.add(meshBrick3);
-        meshBrick1.position.set(position.X - (palletSize.X * 0.45), position.Y - (palletSize.Y * 0.5), position.Z);
-        meshBrick2.position.set(position.X, position.Y - (palletSize.Y * 0.5), position.Z);
-        meshBrick3.position.set(position.X + (palletSize.X * 0.45), position.Y - (palletSize.Y * 0.5), position.Z);        
+        var geometryBrick = new THREE.BoxGeometry(palletSize.X * 0.1, palletSize.Y * 0.9, palletSize.Z * 0.1);
+        var brickPosX = position.X - (palletSize.X * 0.45);
+        var brickPosY = position.Y - (palletSize.Y * 0.5);
+        var brickPosZ = position.Z - (palletSize.Z * 0.45);
+
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 3; j++) {
+                var meshBrick = new THREE.Mesh(geometryBrick, material);
+                scene.add(meshBrick);
+                meshTop.add(meshBrick);
+                meshBrick.position.set(brickPosX, brickPosY, brickPosZ);
+                brickPosX += palletSize.X * 0.45;
+            }
+            brickPosX = position.X - (palletSize.X * 0.45);
+            brickPosZ += (palletSize.Z * 0.45);;
+        }    
     }
 
     init();
-    animate();
+    //animate();
 
     document.onclick = function () {
         if (isAnimationOn) {

d6999f80b355b81f34f115d9cd42a190899f9653
Arkadiusz Pokrzywa <arek09@gmail.com>
2018-10-11 16:13:25 +0100

    Added createAllBoxes

--- PalletConfig.Web/wwwroot/js/canvas.js
+++ PalletConfig.Web/wwwroot/js/canvas.js
@@ -3,12 +3,25 @@
         return window.requestAnimationFrame;
     })();
 
-    var inputSize = new Coordinates(120, 15, 100);
-    
+    // Input data
+    var inputPalletSize = new Coordinates(100, 15, 150);
+    var inputBoxSize = new Coordinates(19, 10, 14);
+    var inputRowsPerLayer = 5;
+    var inputColumnsPerLayer = 10;
+    var inputLayersQuantity = 7;
+
+    // Helpers
+    var inputSizeMax = Math.max(inputPalletSize.X, inputPalletSize.Y, inputPalletSize.Z)
     var camera, scene, renderer, controls;
     var meshTop; //parent of all objects
     var position = new Coordinates(0, 0, 0);
-    var palletSize = calculatePalletSize(inputSize);
+    var palletSize = sizeScalling(inputPalletSize);
+    var boxSize = sizeScalling(inputBoxSize);
+    var zeroPosition = new Coordinates(
+        position.X - (palletSize.X * 0.5) + (boxSize.X * 0.5),
+        position.Y + (palletSize.Y * 0.025) + (boxSize.Y * 0.5),
+        position.Z - (palletSize.Z * 0.5) + (boxSize.Z * 0.5)
+    );
 
     function init() {
         var canvas = document.getElementById('canvas');
@@ -18,6 +31,8 @@
         // Set camera
         camera = new THREE.PerspectiveCamera(70, canvasWidth / canvasHeight, 0.01, 100);
         camera.position.z = 2;
+        camera.position.y = 0.5;
+
 
         // Set scene
         scene = new THREE.Scene();
@@ -34,13 +49,17 @@
         light3 = new THREE.AmbientLight(0x404040, 2);
         scene.add(light3);
 
+        // Create objects
         createPallet();
-        camera.position.x = 1;
-        camera.position.y = 0.5;
+        createAllBoxes(inputRowsPerLayer, inputColumnsPerLayer, zeroPosition, boxSize, inputLayersQuantity);
 
+        // Controls
         controls = new THREE.OrbitControls(camera);
         controls.addEventListener('change', renderer);
+        controls.target.set(0, 0.5, 0);
 
+
+        // Rendering
         renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         renderer.setSize(canvasWidth, canvasHeight);
         canvas.appendChild(renderer.domElement);    
@@ -97,12 +116,41 @@
         }    
     }
 
-    function calculatePalletSize(_inputSize) {
-        var inputSizeMax = Math.max(_inputSize.X, _inputSize.Y, _inputSize.Z)
+    function sizeScalling(_inputSize) {
         _inputSize.X = (_inputSize.X / inputSizeMax) * 2;
         _inputSize.Y = (_inputSize.Y / inputSizeMax) * 2;
         _inputSize.Z = (_inputSize.Z / inputSizeMax) * 2;
         return _inputSize;
+    }
+
+    function createAllBoxes(_numberRows, _numberColumns, _position, _boxSize, _layersQuantity) {
+        var posX = _position.X;
+        var posY = _position.Y;
+        var posZ = _position.Z;
+        
+        for (var i = 0; i < _layersQuantity; i++) {
+            generateLayer(_numberRows, _numberColumns, new Coordinates(X = posX, Y = posY, Z = posZ), _boxSize);
+            posX = _position.X;
+            posY += _boxSize.Y;
+            posZ = _position.Z;
+        }
+    }
+
+    function generateLayer(_numberRows, _numberColumns, _position, _boxSize) {
+        for (var i = 0; i < _numberRows; i++) {
+            for (var j = 0; j < _numberColumns; j++) {
+                generateBox(_boxSize, _position.X + (_boxSize.X * i), _position.Y, _position.Z + (_boxSize.Z * j));
+            }
+        }
+    }
+
+    function generateBox(_boxSize, posX, posY, posZ) {
+        var geometryBox = new THREE.BoxGeometry(_boxSize.X, _boxSize.Y, _boxSize.Z);
+        var materialBox = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('images/boxTexture.jpg'), color: 0xb38600 });
+        var meshBox = new THREE.Mesh(geometryBox, materialBox);
+        scene.add(meshBox);
+        meshTop.add(meshBox);
+        meshBox.position.set(posX, posY, posZ);
     }
 
     init();
---
+++ PalletConfig.Web/wwwroot/js/generateBoxes.js
@@ -0,0 +1,8 @@
+﻿//function generateBox(_boxSize, _position) {
+//    var geometryBox = new THREE.BoxGeometry(_boxSize.X, _boxSize.Y, _boxSize.Z);
+//    var material = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('images/boxTexture.jpg') });
+//    var mesh = new THREE.Mesh(geometryBox, material);
+//    scene.add(mesh);
+//    meshTop.add(mesh);
+//    mesh.position.set(_position.X, _position.Y, _position.Z);
+//}

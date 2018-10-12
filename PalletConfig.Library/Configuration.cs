5e2a7e4d11de2d1fcc8374f44415043f9d38c85c
Arkadiusz Pokrzywa <arek09@gmail.com>
2018-10-12 10:11:24 +0100

    Configuratuin properties added

--- PalletConfig.Library/Configuration.cs
+++ PalletConfig.Library/Configuration.cs
@@ -7,5 +7,12 @@ namespace PalletConfig.Library
     public class Configuration
     {
         //TODO: Create new model which will store pallet configuration and logic to calculate stacking
+        public string OptionId { get; set; }
+        public int LayersQuantity { get; set; }
+        public int RowsPerLayer { get; set; }
+        public int ColumnsPerLayer { get; set; }
+
+
+
     }
 }

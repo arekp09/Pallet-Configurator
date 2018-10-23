f95a801045412fc9a2cb6e5f0927e0570f3afdd1
Arkadiusz Pokrzywa <arek09@gmail.com>
2018-10-23 08:16:52 +0100

    New model added to manage all information in single JSON post (coordinates of Box and Pallet)

--- PalletConfig.Web/Models/ConfigurationModel.cs
+++ PalletConfig.Web/Models/ConfigurationModel.cs
@@ -16,6 +16,8 @@ namespace PalletConfig.Web.Models
         public int NumberOfBoxes { get; set; }
         public double TotalWeight { get; set; }
         public double TotalHeight { get; set; }
+        public CoordinatesModel PalletSize { get; set; }
+        public CoordinatesModel BoxSize { get; set; }
 
         public List<ConfigurationModel> CalculatePalletConfiguration(PalletModel model)
         {
@@ -32,64 +34,49 @@ namespace PalletConfig.Web.Models
         /// <returns></returns>
         public ConfigurationModel CalculateOptionA(PalletModel model)
         {
-            var _option = new ConfigurationModel
+            var _option = new ConfigurationModel();
+
+            _option.PalletSize = new CoordinatesModel
             {
-                OptionName = "Option A",
-                RowsPerLayer = model.PalletSizeX / model.BoxSizeX,
-                ColumnsPerLayer = model.PalletSizeZ / model.BoxSizeZ
+                X = model.PalletSizeX,
+                Y = model.PalletSizeY,
+                Z = model.PalletSizeZ
             };
+
+            _option.BoxSize = new CoordinatesModel
+            {
+                X = model.BoxSizeX,
+                Y = model.BoxSizeY,
+                Z = model.BoxSizeZ
+            };
+
+            _option.OptionName = "Option A";
+            _option.RowsPerLayer = _option.PalletSize.X / model.BoxSizeX;
+            _option.ColumnsPerLayer = _option.PalletSize.Z / model.BoxSizeZ;
 
             int maxBoxesQuantity = Convert.ToInt32(model.PalletWeight / model.BoxWeight);
             double weightPerLayer = _option.RowsPerLayer * _option.ColumnsPerLayer * model.BoxWeight;
             int maxWeightLayersQuantity = Convert.ToInt32(model.PalletWeight / weightPerLayer);
-            int maxHeightLayersQuantity = (model.PalletHeight - model.PalletSizeY) / model.BoxSizeY;
+            int maxHeightLayersQuantity = (model.PalletHeight - _option.PalletSize.Y) / model.BoxSizeY;
             _option.LayersQuantity = Math.Min(maxHeightLayersQuantity, maxWeightLayersQuantity);
 
-            var maxVolume = model.PalletSizeX * model.PalletSizeZ * (model.PalletHeight - model.PalletSizeY);
+            var maxVolume = _option.PalletSize.X * _option.PalletSize.Z * (model.PalletHeight - _option.PalletSize.Y);
             var boxVolume = model.BoxSizeX * model.BoxSizeY * model.BoxSizeZ;
             _option.NumberOfBoxes = _option.RowsPerLayer * _option.ColumnsPerLayer * _option.LayersQuantity;
             var actualVolume = _option.NumberOfBoxes * boxVolume;
             _option.Volume = Convert.ToDouble(actualVolume) / Convert.ToDouble(maxVolume);
 
-            _option.TotalHeight = model.PalletSizeY + (model.BoxSizeY * _option.LayersQuantity);
+            _option.TotalHeight = _option.PalletSize.Y + (model.BoxSizeY * _option.LayersQuantity);
             _option.TotalWeight = _option.NumberOfBoxes * model.BoxWeight;
 
             return _option;
         }
+
         /// <summary>
         /// Calculate Option B - setup with all boxes moved around 90 deg
         /// </summary>
         /// <param name="model">Model received from user input</param>
         /// <returns></returns>
-        public ConfigurationModel CalculateOptionB(PalletModel model)
-        {
-            var originalXLength = model.BoxSizeX;
-            model.BoxSizeX = model.BoxSizeZ;
-            model.BoxSizeZ = originalXLength;
-
-            var _option = new ConfigurationModel
-            {
-                OptionName = "Option B",
-                RowsPerLayer = model.PalletSizeX / model.BoxSizeX,
-                ColumnsPerLayer = model.PalletSizeZ / model.BoxSizeZ
-            };
-
-            int maxBoxesQuantity = Convert.ToInt32(model.PalletWeight / model.BoxWeight);
-            double weightPerLayer = _option.RowsPerLayer * _option.ColumnsPerLayer * model.BoxWeight;
-            int maxWeightLayersQuantity = Convert.ToInt32(model.PalletWeight / weightPerLayer);
-            int maxHeightLayersQuantity = (model.PalletHeight - model.PalletSizeY) / model.BoxSizeY;
-            _option.LayersQuantity = Math.Min(maxHeightLayersQuantity, maxWeightLayersQuantity);
-
-            var maxVolume = model.PalletSizeX * model.PalletSizeZ * (model.PalletHeight - model.PalletSizeY);
-            var boxVolume = model.BoxSizeX * model.BoxSizeY * model.BoxSizeZ;
-            _option.NumberOfBoxes = _option.RowsPerLayer * _option.ColumnsPerLayer * _option.LayersQuantity;
-            var actualVolume = _option.NumberOfBoxes * boxVolume;
-            _option.Volume = Convert.ToDouble(actualVolume) / Convert.ToDouble(maxVolume);
-
-            _option.TotalHeight = model.PalletSizeY + (model.BoxSizeY * _option.LayersQuantity);
-            _option.TotalWeight = _option.NumberOfBoxes * model.BoxWeight;
-
-            return _option;
-        }
+        
     }
 }
---
+++ PalletConfig.Web/Models/CoordinatesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalletConfig.Web.Models
+{
+    public class CoordinatesModel
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}

f38e53a400f1bbd2614828ae3a1b909935f791a4
Arkadiusz Pokrzywa <arek09@gmail.com>
2018-10-24 17:00:08 +0100

    Test CalculateOption added (Option E with rotation 50%)

--- PalletConfig.Tests/ConfigurationTests.cs
+++ PalletConfig.Tests/ConfigurationTests.cs
@@ -13,16 +13,30 @@ namespace PalletConfig.Tests
             BoxWeight = 2, PalletSizeX = 150, PalletSizeY = 25, PalletSizeZ = 250,
             PalletHeight = 200, PalletWeight = 500, StackOpposite = false };
 
-        readonly StackingOptionModel stackingOptionModel = new StackingOptionModel { Name = "Option A", Rotation = 1, Mode = "Round" };
-        
+        readonly StackingOptionModel stackingOptionModel_A = new StackingOptionModel { Name = "Option A", Rotation = 1, Mode = "Round" };
+        readonly StackingOptionModel stackingOptionModel_E = new StackingOptionModel { Name = "Option E", Rotation = 0.5, Mode = "Round" };
+
         [Fact]
-        public void CalculateOptionA_InputModel_ReturnConfigurationObject()
+        public void CalculateOption_NoRotation_ReturnConfigurationObject()
         {
             var config = new ConfigurationModel();
 
-            var actual = config.CalculateOption(testModel, stackingOptionModel);
+            var actual = config.CalculateOption(testModel, stackingOptionModel_A);
 
             Assert.Equal(8, actual.Standard.ColumnsPerLayer);
+            Assert.Equal(7, actual.Standard.RowsPerLayer);
+            Assert.Equal(4, actual.LayersQuantity);
+        }
+
+        [Fact]
+        public void CalculateOption_Rotation50_ReturnConfigurationObject()
+        {
+            var config = new ConfigurationModel();
+
+            var actual = config.CalculateOption(testModel, stackingOptionModel_E);
+
+            Assert.Equal(4, actual.Standard.ColumnsPerLayer);
+            Assert.Equal(6, actual.Rotated.ColumnsPerLayer);
             Assert.Equal(7, actual.Standard.RowsPerLayer);
             Assert.Equal(4, actual.LayersQuantity);
         }
